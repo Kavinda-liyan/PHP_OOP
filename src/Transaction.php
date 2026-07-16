80b2145f91044572b8ede6b54ff91ecf1b82aa71
Kavinda <chamarakavinda@hotmail.com>
2026-07-16 16:28:30 +0530

    Refactor Transaction class to use typed properties and improve method structure

--- src/Transaction.php
+++ src/Transaction.php
@@ -1,6 +1,20 @@
 <?php
+
+declare(strict_types=1);
 class Transaction
 {
-    public $amount;
-    public $description;
+    public float $amount;
+    public string $description;
+
+    function set_deatils($amount, $description)
+    {
+        $this->amount = $amount;
+        $this->description = $description;
+    }
+
+    function get_details()
+    {
+        echo "Amount of you pay is : " . $this->amount . "<br>" .
+            "Account Number : " . $this->description . "<br>";
+    }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -3,5 +3,9 @@
 require_once '../Transaction.php';
 
 $transaction = new Transaction();
+$transaction->amount=15;
+
+$transaction->set_deatils(24000,"COMBANK13185002");
+$transaction->get_details();
 
 var_dump($transaction);
